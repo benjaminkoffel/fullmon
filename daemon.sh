c2d0ede60bea063c34f686aa6d5cfdf0a3536d83
Benjamin Koffel <benjaminkoffel@gmail.com>
2018-10-28 23:45:49 +1100

    update example daemon script with more realistic settings

--- daemon.sh
+++ daemon.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python3 /opt/fullmon/agent.py --auditd /var/log/audit/audit.log --baseline 5 --monitor 10 >> /var/log/fullmon.log
+python3 /opt/fullmon/agent.py --auditd /var/log/audit/audit.log --baseline 3600 --monitor 60 >> /var/log/fullmon.log
