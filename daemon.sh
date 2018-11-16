e7b21c3798acf946e5837b542fa203ea96672b73
Benjamin Koffel <benjaminkoffel@gmail.com>
2018-11-16 19:00:53 +1100

    updated daemon with rebase option

--- daemon.sh
+++ daemon.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python3 /opt/fullmon/agent.py --auditd /var/log/audit/audit.log --baseline 3600 --monitor 60 >> /var/log/fullmon.log
+python3 /opt/fullmon/agent.py --auditd /var/log/audit/audit.log --baseline 3600 --monitor 60 --rebase >> /var/log/fullmon.log
