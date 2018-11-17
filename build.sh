81fe47c6df2db24844c0766b05b6cf3d3491d750
Benjamin Koffel <benjaminkoffel@gmail.com>
2018-11-17 19:39:34 +1100

    update build to package all files

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ set -euxo pipefail
 python3 -m unittest discover -s agent/ -v
 sudo python3 -m pip install nuitka --upgrade
 python3 -m nuitka --follow-imports -o fullmon agent/agent.py
+tar -zcvf fullmon.tar.gz fullmon fullmon.sh fullmon.service fullmon.conf audit.rules
--- daemon.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-python3 /opt/fullmon/agent.py --auditd /var/log/audit/audit.log --baseline 3600 --monitor 60 --rebase >> /var/log/fullmon.log
