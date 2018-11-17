1d7ca5ae0a28259e8865d3a70c80bc9ebc6affaa
Benjamin Koffel <benjaminkoffel@gmail.com>
2018-11-17 18:58:12 +1100

    update ci

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 set -euxo pipefail
 
 python3 -m unittest discover -s agent/ -v
-python3 -m pip install nuitka --upgrade
+sudo python3 -m pip install nuitka --upgrade
 python3 -m nuitka --follow-imports -o fullmon agent/agent.py
