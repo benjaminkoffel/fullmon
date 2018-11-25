6f6945f0e25fa7b77a04514a475a32265665c9fa
debian <benjaminkoffel@gmail.com>
2018-11-25 16:54:12 +1100

    upd build script

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 set -eux
 
 python3 -m unittest discover -s agent/ -v
-sudo python3 -m pip install nuitka --upgrade
+python3 -m pip install --user nuitka --upgrade
 python3 -m nuitka --follow-imports -o fullmon agent/agent.py
 tar -zcvf fullmon.tar.gz fullmon fullmon.sh fullmon.service fullmon.conf audit.rules install-debian.sh
